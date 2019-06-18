6aaaced1c51df91d5ffe8c04eac7dcb6e1602037
Charlotte Shih <shih.charlotte@gmail.com>
2019-06-17 18:58:51 -0700

    Add functionality to AddRecipeForm

--- src/components/AddRecipeForm/AddRecipeForm.js
+++ src/components/AddRecipeForm/AddRecipeForm.js
@@ -3,16 +3,40 @@ import RecipeContext from '../../contexts/RecipeContext'
 import { Button, Input } from '../Utils/Utils'
 import './AddRecipeForm.css'
 
-import FolderDropDown from '../FolderDropDown/FolderDropDown'
-
 class AddRecipeForm extends Component {
 	static contextType = RecipeContext
 
 	state = {
 		title: '',
-		ingredients: [],
-		steps: [],
-		folder_id: null
+		ingredients: [
+			{
+				id: 1,
+				item: ''
+			},
+			{
+				id: 2,
+				item: ''
+			},
+			{
+				id: 3,
+				item: ''
+			}
+		],
+		steps: [
+			{
+				id: 1,
+				step: ''
+			},
+			{
+				id: 2,
+				step: ''
+			},
+			{
+				id: 3,
+				step: ''
+			}
+		],
+		folder_id: '',
 	}
 
 	handleTitleChange = e => {
@@ -21,13 +45,33 @@ class AddRecipeForm extends Component {
 		})
 	}
 
+	handleSelectFolder = e => {
+		this.setState({
+			folder_id: e.target.value
+		})
+	}
+
+	handleIngredientInput = e => {
+		let ing_id = e.target.id
+		let ing_item = e.target.value
+
+		this.setState({
+			ingredients: [
+				{
+					id: ing_id,
+					item: ing_item
+				}
+			]
+		})
+	}
+
 	addIngredientInput = e => {
 		e.preventDefault()
-		this.setState((prevState) => ({
+		this.setState(prevState => ({
 			ingredients: [
 				...prevState.ingredients,
 				{
-					id: prevState.ingredients.length,
+					id: prevState.ingredients.length + 1,
 					item: ''
 				}
 			]
@@ -36,22 +80,36 @@ class AddRecipeForm extends Component {
 
 	addStepInput = e => {
 		e.preventDefault()
-		this.setState((prevState) => ({
+		this.setState(prevState => ({
 			steps: [
 				...prevState.steps,
 				{
-					id: prevState.steps.length,
+					id: prevState.steps.length + 1,
 					step: ''
 				}
 			]
 		}))
 	}
 
+	clearForm = () => {
+    this.setState({
+      folderName: ''
+    })
+  }
+
+	handleSubmitRecipe = e => {
+		e.preventDefault()
+		console.log(this.state)
+		this.clearForm()
+	}
+
 	render() {
 		let { ingredients, steps } = this.state
 
 		return (
-			<form className="add-recipe-form">
+			<form
+				className="add-recipe-form"
+				onSubmit={this.handleSubmitRecipe}>
 				<div className="title">
 					<label htmlFor="title">Title: </label>
 					<Input
@@ -64,38 +122,22 @@ class AddRecipeForm extends Component {
 
 				<div className="ingredients">
 					<label htmlFor="ingredients">Ingredients: </label>
-					<Input
-						type="text"
-						key="1"
-						id="recipe_ingredients"
-						name="recipe_ingredients"
-						required />
-					<Input
-						type="text"
-						key="2"
-						id="recipe_ingredients"
-						name="recipe_ingredients" />
-					<Input
-						type="text"
-						key="3"
-						id="recipe_ingredients"
-						name="recipe_ingredients" />
-					<Input
-						type="text"
-						key="4"
-						id="recipe_ingredients"
-						name="recipe_ingredients" />
+					<ul>
 					{
 						ingredients.map(ing => {
 							return (
-								<Input
-									type="text"
-									key={ing.id + 5}
-									id="recipe_ingredients"
-									name="recipe_ingredients" />
+								<li>
+									<Input
+										onChange={this.handleIngredientInput}
+										type="text"
+										key={ing.id}
+										id={`${ing.id}`}
+										name="recipe_ingredients" />
+								</li>
 							)
 						})
 					}
+					</ul>
 					<Button
 						onClick={this.addIngredientInput}
 						className="add-ingredient-button">
@@ -105,38 +147,21 @@ class AddRecipeForm extends Component {
 
 				<div className="steps">
 					<label htmlFor="steps">Steps: </label>
-					<Input
-						type="text"
-						key="1"
-						id="recipe_steps"
-						name="recipe_steps"
-						required />
-					<Input
-						type="text"
-						key="2"
-						id="recipe_steps"
-						name="recipe_steps" />
-					<Input
-						type="text"
-						key="3"
-						id="recipe_steps"
-						name="recipe_steps" />
-					<Input
-						type="text"
-						key="4"
-						id="recipe_steps"
-						name="recipe_steps" />
+					<ol>
 					{
 						steps.map(step => {
 							return (
-								<Input
-									type="text"
-									key={step.id + 5}
-									id="recipe_steps"
-									name="recipe_steps" />
+								<li>
+									<Input
+										type="text"
+										key={step.id}
+										id={`${step.id}`}
+										name="recipe_steps" />
+								</li>
 							)
 						})
 					}
+					</ol>
 					<Button
 						onClick={this.addStepInput}
 						className="add-step-button">
@@ -146,10 +171,25 @@ class AddRecipeForm extends Component {
 
 				<div className="folders">
 					<label htmlFor="folders">Folder: </label>
-					<FolderDropDown />
+					<select className="folder-dropdown" onChange={this.handleSelectFolder}>
+						<option>Please Select a Folder...</option>
+						{
+							this.context.folders.map(folder => {
+								return (
+									<option
+										key={folder.id}
+										value={`${folder.id}`}>
+										{folder.folderName}
+									</option>
+								)
+							})
+						}
+					</select>
 				</div>
 
-				<Button type="submit">Create New Recipe</Button>
+				<Button type="submit">
+						Create New Recipe
+				</Button>
 				<Button type="reset">Clear</Button>
 			</form>
 		)
