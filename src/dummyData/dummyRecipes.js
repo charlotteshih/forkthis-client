e844aba9cfe61596993bd77b0778276bd8500579
Charlotte Shih <shih.charlotte@gmail.com>
2019-06-18 21:32:41 -0700

    Populates dummy data

--- src/dummyData/dummyRecipes.js
+++ src/dummyData/dummyRecipes.js
@@ -1,39 +1,63 @@
 let recipes = [
   {
     id: 1,
-    name: 'Pecan Pie',
+    name: 'Key Lime Pie',
     folder_id: 4,
     ingredients: [
       {
         id: 1,
-        item: 'Item 1'
+        item: '1 1/4 cups graham cracker crumbs from 9 (2 1/4-inch by 4 3/4-inch) crackers'
       },
       {
         id: 2,
-        item: 'Item 2'
+        item: '2 tablespoons sugar'
       },
       {
         id: 3,
-        item: 'Item 3'
+        item: '5 tablespoons unsalted butter, melted'
       },
+      {
+        id: 4,
+        item: '1 (14-ounce) can sweetened condensed milk'
+      },
+      {
+        id: 5,
+        item: '4 large egg yolks'
+      },
+      {
+        id: 6,
+        item: '1/2 cup plus 2 tablespoons fresh or bottled Key lime juice'
+      },
+      {
+        id: 7,
+        item: '3/4 cup chilled heavy cream'
+      }
     ],
     steps: [
       {
         id: 1,
-        step: 'Step 1'
+        step: 'Preheat oven to 350°F.'
       },
       {
         id: 2,
-        step: 'Step 2'
+        step: 'Stir together graham cracker crumbs, sugar, and butter in a bowl with a fork until combined well, then press mixture evenly onto bottom and up side of a 9-inch (4-cup) glass pie plate.'
       },
       {
         id: 3,
-        step: 'Step 3'
+        step: 'Bake crust in middle of oven 10 minutes and cool in pie plate on a rack. Leave oven on.'
       },
       {
         id: 4,
-        step: 'Step 4'
+        step: 'Whisk together condensed milk and yolks in a bowl until combined well. Add juice and whisk until combined well (mixture will thicken slightly).'
       },
+      {
+        id: 5,
+        step: 'Pour filling into crust and bake in middle of oven 15 minutes. Cool pie completely on rack (filling will set as it cools), then chill, covered, at least 8 hours.'
+      },
+      {
+        id: 6,
+        step: 'Just before serving, beat cream in a bowl with an electric mixer until it just holds stiff peaks. Serve pie topped with cream.'
+      }
     ]
   },
   {
@@ -43,34 +67,66 @@ let recipes = [
     ingredients: [
       {
         id: 1,
-        item: 'Item 1'
+        item: '1½ cups (200 g) all-purpose flour (spooning into measuring cups, then leveling)'
       },
       {
         id: 2,
-        item: 'Item 2'
+        item: '1¼ tsp. (4 g) Diamond Crystal or ¾ tsp. (4 g) Morton kosher salt'
       },
       {
         id: 3,
-        item: ''
+        item: '¾ tsp. (4 g) baking soda'
       },
+      {
+        id: 4,
+        item: '¾ cup (1½ sticks; 169 g) unsalted butter, divided'
+      },
+      {
+        id: 5,
+        item: '1 cup (200 g) (packed) dark brown sugar'
+      },
+      {
+        id: 6,
+        item: '¼ cup (50 g) granulated sugar'
+      },
+      {
+        id: 7,
+        item: '1 large egg'
+      },
+      {
+        id: 8,
+        item: '2 large egg yolks'
+      },
+      {
+        id: 9,
+        item: '2 tsp. vanilla extract'
+      },
+      {
+        id: 10,
+        item: '6 oz. (170 g) bittersweet chocolate (60%–70% cacao), coarsely chopped, or semisweet chocolate chips'
+      }
     ],
     steps: [
       {
         id: 1,
-        step: 'Step 1'
+        step: 'Place racks in upper and lower thirds of oven; preheat to 375°.'
       },
       {
         id: 2,
-        step: 'Step 2'
+        step: 'Whisk flour, salt, and baking soda in a small bowl; set aside.'
       },
       {
         id: 3,
-        step: 'Step 3'
+        step: 'Cook ½ cup (1 stick; 113 g) butter in a large saucepan over medium heat, swirling often and scraping bottom of pan with a heatproof rubber spatula, until butter foams, then browns, about 4 minutes. Transfer butter to a large heatproof bowl and let cool 1 minute. Cut remaining ¼ cup (½ stick; 56 g) butter into small pieces and add to brown butter (it should start to melt but not foam and sizzle, so test with one piece before adding the rest).'
       },
       {
         id: 4,
-        step: 'Step 4'
+        step: 'Once butter is melted, add both sugars and whisk, breaking up any clumps, until sugar is incorporated and no lumps remain. Add egg and egg yolks and whisk until sugar dissolves and mixture is smooth, about 30 seconds. Whisk in vanilla. Using rubber spatula, fold reserved dry ingredients into butter mixture just until no dry spots remain, then fold in chocolate (the dough will be soft but should hold its shape once scooped; if it slumps or oozes after being scooped, stir dough back together several times and let rest 5–10 minutes until scoops hold their shape as the flour hydrates).'
       },
+      {
+        id: 5,
+        step: 'Using a 1½-oz. scoop (3 Tbsp.), portion out 16 balls of dough and divide between 2 parchment-lined rimmed baking sheets. Bake cookies, rotating sheets if cookies are browning very unevenly (otherwise, just leave them alone), until deep golden brown and firm around the edges, 8–10 minutes. Let cool on baking sheets.'
+      }
     ]
   },
   {
@@ -80,33 +136,89 @@ let recipes = [
     ingredients: [
       {
         id: 1,
-        item: 'Item 1'
+        item: '1 tablespoon olive oil'
       },
       {
         id: 2,
-        item: 'Item 2'
+        item: '2 large yellow onions, diced'
       },
       {
         id: 3,
-        item: 'Item 3'
+        item: '1 green pepper (capsicum), diced'
       },
+      {
+        id: 4,
+        item: '1-2 jalapeño peppers finely chopped (with seeds)'
+      },
+      {
+        id: 5,
+        item: '8 cloves garlic, chopped or minced (or 2 tablespoons minced garlic)'
+      },
+      {
+        id: 6,
+        item: '2 pounds (1 kg) ground beef mince'
+      },
+      {
+        id: 7,
+        item: '2 tablespoons ground cumin'
+      },
+      {
+        id: 8,
+        item: '1-2 tablespoons chili powder (adjust to your tastes)'
+      },
+      {
+        id: 9,
+        item: '2 teaspoons smoked paprika (or sweet paprika)'
+      },
+      {
+        id: 10,
+        item: '1 teaspoon salt'
+      },
+      {
+        id: 11,
+        item: '1⁄2 teaspoon cayenne (adjust to your tastes)'
+      },
+      {
+        id: 12,
+        item: '14 ounces (410 g) can crushed tomatoes'
+      },
+      {
+        id: 13,
+        item: '15 ounces (420 g) can tomato sauce'
+      },
+      {
+        id: 14,
+        item: '2 cups (500 ml) beef or chicken broth'
+      },
+      {
+        id: 15,
+        item: '28 ounces (800 g) cans kidney beans, drained'
+      },
+      {
+        id: 16,
+        item: '14 ounces (400 g) can black beans, drained'
+      },
+      {
+        id: 17,
+        item: '(Optional Toppings) sour cream, grated cheddar cheese, sliced jalapenos, chopped green onions, fresh cilantro leaves'
+      }
     ],
     steps: [
       {
         id: 1,
-        step: 'Step 1'
+        step: 'Heat oil in large heavy-based pot (or casserole pot) over medium-high heat. Add onions and sauté until transparent, then add the green peppers (or capsicum), jalapeños and garlic; sautéing for a further minute, until the garlic is fragrant.'
       },
       {
         id: 2,
-        step: 'Step 2'
+        step: 'Add beef and cook until browned, breaking up any big chunks with the end of your wooden spoon. Stir in the cumin, chili powder, paprika and salt and mix through until beef is fully coated in the seasonings.'
       },
       {
         id: 3,
-        step: 'Step 3'
+        step: 'Mix in the crushed tomatoes along with the juices, the tomato sauce (or soup), broth and beans. Bring to a boil, then lower heat and allow to simmer while stirring occasionally, until the chili thickens (about 30 minutes).'
       },
       {
         id: 4,
-        step: 'Step 4'
+        step: 'Ladle into individual bowls, and top with sour cream, grated cheese, sliced jalapeños, chopped green onions, and/or cilantro.'
       },
     ]
   },
@@ -117,34 +229,54 @@ let recipes = [
     ingredients: [
       {
         id: 1,
-        item: 'Item 1'
+        item: '1 box pasta (penne or fusilli)'
       },
       {
         id: 2,
-        item: 'Item 2'
+        item: '1 head broccoli, diced into bite-sized morsels'
       },
       {
         id: 3,
-        item: 'Item 3'
+        item: '1 cup mushrooms, sliced'
       },
+      {
+        id: 4,
+        item: '1/3 cup sun-dried tomatoes, sliced (with oil marinade)'
+      },
+      {
+        id: 5,
+        item: '1 cup Pepper Jack cheese, diced into bite-sized cubes'
+      },
+      {
+        id: 6,
+        item: '1/4 cup roasted pine nuts'
+      },
+      {
+        id: 7,
+        item: 'Salt and pepper (to taste)'
+      }
     ],
     steps: [
       {
         id: 1,
-        step: 'Step 1'
+        step: 'Cook pasta according to specifications on the box.'
       },
       {
         id: 2,
-        step: 'Step 2'
+        step: 'While pasta is cooking, saute broccoli and mushrooms.'
       },
       {
         id: 3,
-        step: 'Step 3'
+        step: 'Drain pasta and toss with Pepper Jack cheese while still hot, so that the cheese melts slightly.'
       },
       {
         id: 4,
-        step: 'Step 4'
+        step: 'Mix in sun-dried tomatoes, broccoli, mushrooms, and pine nuts. Salt and pepper to taste. For extra flavor, mix in the oil marinade that comes with sun-dried tomatoes.'
       },
+      {
+        id: 5,
+        step: 'Enjoy!'
+      }
     ]
   },
   {
@@ -154,34 +286,62 @@ let recipes = [
     ingredients: [
       {
         id: 1,
-        item: 'Item 1'
+        item: '2 cups rice'
       },
       {
         id: 2,
-        item: 'Item 2'
+        item: '2 eggs'
       },
       {
         id: 3,
-        item: 'Item 3'
+        item: '1 can Spam, diced'
       },
+      {
+        id: 4,
+        item: '1 cup frozen veggies'
+      },
+      {
+        id: 5,
+        item: 'Soy sauce - to taste'
+      },
+      {
+        id: 6,
+        item: 'Ginger - to taste'
+      },
+      {
+        id: 7,
+        item: 'Salt & pepper - to taste'
+      },
+      {
+        id: 8,
+        item: '(Optional) Roasted sesame seeds'
+      },
+      {
+        id: 9,
+        item: '(Optional) Freshly sliced green onion'
+      }
     ],
     steps: [
       {
         id: 1,
-        step: 'Step 1'
+        step: 'In a large pan, toss diced Spam cubes with soy sauce until edges become crispy - about 5 minutes. Set aside.'
       },
       {
         id: 2,
-        step: 'Step 2'
+        step: 'Scramble eggs and cook in same pan.'
       },
       {
         id: 3,
-        step: 'Step 3'
+        step: 'Add Spam and frozen veggies to eggs and toss until well-incorporated and veggies are thawed.'
       },
       {
         id: 4,
-        step: 'Step 4'
+        step: 'Add rice into pan and toss until well-incorporated, seasoning to taste with soy sauce, ginger, and salt and pepper.'
       },
+      {
+        id: 5,
+        step: 'Top with roasted sesame seeds and a handful of freshly sliced green onions, and enjoy!'
+      }
     ]
   },
   {
@@ -191,33 +351,33 @@ let recipes = [
     ingredients: [
       {
         id: 1,
-        item: 'Item 1'
+        item: '2 eggs'
       },
       {
         id: 2,
-        item: 'Item 2'
+        item: '3 Tbsp. olive oil'
       },
       {
         id: 3,
-        item: 'Item 3'
+        item: '1/4 cups milk'
       },
+      {
+        id: 4,
+        item: 'Salt and pepper (to taste)'
+      }
     ],
     steps: [
       {
         id: 1,
-        step: 'Step 1'
+        step: 'Heat olive oil in pan on low heat until shimmering. Meanwhile, crack eggs into a bowl, and whip until foamy.'
       },
       {
         id: 2,
-        step: 'Step 2'
+        step: 'Add egg-and-milk batter to pan. Stir constantly, taking care not to let eggs burn.'
       },
       {
         id: 3,
-        step: 'Step 3'
-      },
-      {
-        id: 4,
-        step: 'Step 4'
+        step: 'When eggs are cooked through but not rubbery, remove from heat and salt and pepper to taste.'
       },
     ]
   },
