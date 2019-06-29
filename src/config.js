7d74a46f31a7d8fc5f3f52941f04cfeb295d6606
Charlotte Shih <shih.charlotte@gmail.com>
2019-06-29 01:13:32 -0700

    Hook up backend

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://pure-escarpment-63874.herokuapp.com/api',
+  API_ENDPOINT: 'https://peaceful-reaches-42136.herokuapp.com/api',
   API_KEY: process.env.REACT_APP_API_KEY,
 }
